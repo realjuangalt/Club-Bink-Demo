2b90dc8d9ff103f66f8e6a565bf4c704519ec129
Juan Galt <145779585+realjuangalt@users.noreply.github.com>
2025-02-18 16:16:29 -0500

    Replace repository contents with new files

--- demo/components/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-screen bg-[#1C1C1C] flex items-center justify-center">
-      <div className="text-white text-2xl">Loading dashboard...</div>
-    </div>
-  )
-}
-
--- demo/components/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { LanguageProvider } from '../contexts/LanguageContext'
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <LanguageProvider>{children}</LanguageProvider>
-}
-
